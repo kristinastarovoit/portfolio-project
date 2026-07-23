ee9216fab51fdbf6e1709564f8b2867f74606dde
kristina <kristina.starovoit@gmail.com>
2026-07-23 13:56:04 +0200

    add: basic html structure for projects

--- src/app/app.ts
+++ src/app/app.ts
@@ -5,10 +5,11 @@ import { Footer } from "./layout/footer/footer";
 import { Hero } from "./pages/main/components/hero/hero";
 import { About } from "./pages/main/components/about/about";
 import { Skills } from "./pages/main/components/skills/skills";
+import { Projects } from "./pages/main/components/projects/projects";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Hero, About, Skills],
+  imports: [RouterOutlet, Header, Footer, Hero, About, Skills, Projects],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
