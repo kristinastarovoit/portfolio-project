28ef72bd55c718e7c9cf5bd1d51b0490d1d84a97
kristina <kristina.starovoit@gmail.com>
2026-07-23 13:45:04 +0200

    add: basic html structure for hero

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
 import { Footer } from "./layout/footer/footer";
+import { Hero } from "./pages/main/components/hero/hero";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer],
+  imports: [RouterOutlet, Header, Footer, Hero],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
