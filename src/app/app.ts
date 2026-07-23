7a52b013c9a36e8da03b013593ecb621555acff7
kristina <kristina.starovoit@gmail.com>
2026-07-23 13:49:02 +0200

    add: basic html structure for about me

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from "./layout/header/header";
 import { Footer } from "./layout/footer/footer";
 import { Hero } from "./pages/main/components/hero/hero";
+import { About } from "./pages/main/components/about/about";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Hero],
+  imports: [RouterOutlet, Header, Footer, Hero, About],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
