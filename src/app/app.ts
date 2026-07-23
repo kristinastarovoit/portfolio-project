32e3005b7852ad962e71ae6150b16175b2bf2d31
kristina <kristina.starovoit@gmail.com>
2026-07-23 13:52:10 +0200

    add: basic html structure for skills

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,10 +4,11 @@ import { Header } from "./layout/header/header";
 import { Footer } from "./layout/footer/footer";
 import { Hero } from "./pages/main/components/hero/hero";
 import { About } from "./pages/main/components/about/about";
+import { Skills } from "./pages/main/components/skills/skills";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, Footer, Hero, About],
+  imports: [RouterOutlet, Header, Footer, Hero, About, Skills],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
