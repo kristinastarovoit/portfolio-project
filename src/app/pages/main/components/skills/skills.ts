29a998d0a6b7d55952f10e3f43dec380e36e6055
kristina <kristina.starovoit@gmail.com>
2026-07-27 13:38:55 +0200

    Add: skill icons in for loop and basic styling for icon section

--- src/app/pages/main/components/skills/skills.ts
+++ src/app/pages/main/components/skills/skills.ts
@@ -6,4 +6,43 @@ import { Component } from '@angular/core';
   templateUrl: './skills.html',
   styleUrl: './skills.scss',
 })
-export class Skills {}
+export class Skills {
+  skills = [
+    {
+      name: 'HTML',
+      img: '/icons/skill_html.png'
+    },
+    {
+      name: 'CSS',
+      img: '/icons/skill_css.png'
+    },
+    {
+      name: 'JavaScript',
+      img: '/icons/skill_js.png'
+    },
+    {
+      name: 'TypeScript',
+      img: '/icons/skill_ts.png'
+    },
+    {
+      name: 'Angular',
+      img: '/icons/skill_angular.png'
+    },
+    {
+      name: 'SASS',
+      img: '/icons/skill_sass.png'
+    },
+    {
+      name: 'Git',
+      img: '/icons/skill_git.png'
+    },
+    {
+      name: 'REST-API',
+      img: '/icons/skill_api.png'
+    },
+    {
+      name: 'Scrum',
+      img: '/icons/skill_scrum.png'
+    }
+  ]
+}
