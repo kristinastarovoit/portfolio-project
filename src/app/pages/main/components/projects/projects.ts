714c8323b004a0affb36b650be2203b5db6b47b7
kristina <kristina.starovoit@gmail.com>
2026-07-30 13:04:19 +0200

    add: project render with json and @for

--- src/app/pages/main/components/projects/projects.ts
+++ src/app/pages/main/components/projects/projects.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
-export class Projects {}
+export class Projects {
+  projects = [
+    {
+      name: "Join",
+      description: "Task manager inspired by the Kanban System. Create and organize tasks using drag and drop functions, assign users and categories.",
+      featured: true,
+      img: "/img/join.png"
+    },
+    {
+      name: "El Pollo Loco",
+      description: "Jump, run and throw game based on object-oriented approach. Help Pepe to find coins and tabasco salsa to fight against the crazy hen.",
+      featured: false,
+      img: "/img/elpolloloco.png"
+    },
+    {
+      name: "PokéDex",
+      description: "Interactive Pokédex app fetching dynamic data from the PokéAPI. Browse Pokémon, view detailed stats, types, and abilities in a clean, visual interface.",
+      featured: false,
+      img: "/img/pokedex.png"
+    }
+  ]
+}
