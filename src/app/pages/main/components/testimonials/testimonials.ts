295dfe894212bcbc01b65f9c88efd1db2964b33b
kristina <kristina.starovoit@gmail.com>
2026-07-30 15:37:11 +0200

    add: styling for testimonial component

--- src/app/pages/main/components/testimonials/testimonials.ts
+++ src/app/pages/main/components/testimonials/testimonials.ts
@@ -6,4 +6,25 @@ import { Component } from '@angular/core';
   templateUrl: './testimonials.html',
   styleUrl: './testimonials.scss',
 })
-export class Testimonials {}
+export class Testimonials {
+  testimonials = [
+    {
+      text: "Kristina ist so cool",
+      name: "Max Mustermann",
+      position: "Frontend Dev",
+      className: "testimonials__card--one"
+    },
+    {
+      text: "Kristina ist so toll",
+      name: "Peter Hans",
+      position: "Frontend Dev",
+      className: "testimonials__card--two"
+    },
+    {
+      text: "Kristina ist so gut gebräunt und gutaussehend",
+      name: "Michael Jackson",
+      position: "Legende",
+      className: "testimonials__card--three"
+    }
+  ]
+}
