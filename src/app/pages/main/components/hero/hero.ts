d81cfda2fcc6a1228ec6dbc634378b42ba3cc6f8
kristina <kristina.starovoit@gmail.com>
2026-07-31 12:29:33 +0200

    add: hero title animation

--- src/app/pages/main/components/hero/hero.ts
+++ src/app/pages/main/components/hero/hero.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+interface Letter {
+  char: string;
+  active: boolean;
+}
 
 @Component({
   selector: 'app-hero',
@@ -6,4 +10,25 @@ import { Component } from '@angular/core';
   templateUrl: './hero.html',
   styleUrl: './hero.scss',
 })
-export class Hero {}
+export class Hero {
+  words = signal<Letter[][]>([
+    'Fullstack'.split('').map(char => ({ char, active: false })),
+    'DEVELOPER'.split('').map(char => ({ char, active: false })),
+  ]);
+
+  toggleLetter(word: Letter[], letter: Letter) {
+    letter.active = !letter.active;
+  }
+
+  setActive(letter: Letter, state: boolean) {
+    letter.active = state;
+  }
+
+  displayChar(letter: Letter): string {
+    if (!letter.active) return letter.char;
+    // Groß- und Kleinschreibung umdrehen
+    return letter.char === letter.char.toUpperCase()
+      ? letter.char.toLowerCase()
+      : letter.char.toUpperCase();
+  }
+}
