8f9f5ae1c02186650319b8139eb191bd6cf6b8f7
kristina <kristina.starovoit@gmail.com>
2026-08-01 15:02:40 +0200

    add: next project function for project details

--- src/app/pages/project-details/project-details.ts
+++ src/app/pages/project-details/project-details.ts
@@ -2,6 +2,7 @@ import { Component, input, inject } from '@angular/core';
 import { ProjectsService } from '../../services/projects.service';
 import { Project } from '../../interfaces/project';
 import { Header } from "../../layout/header/header";
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-project-details',
@@ -12,10 +13,19 @@ import { Header } from "../../layout/header/header";
 export class ProjectDetails {
   projectsService = inject(ProjectsService);
   slug = input.required<string>();
+  router = inject(Router);
 
   //gibt das richtige Projekt zurück in abhängigkeit vom slug
 
   project(): Project | undefined {
     return this.projectsService.getBySlug(this.slug());
+  }
+
+  goToNextProject() {
+    const allProjects = this.projectsService.projects();
+    const currentIndex = allProjects.findIndex(p => p.slug === this.slug());
+    const nextIndex = (currentIndex + 1) % allProjects.length;
+    const nextProject = allProjects[nextIndex];
+    this.router.navigate(['/project-detail', nextProject.slug]);
   }
 }
