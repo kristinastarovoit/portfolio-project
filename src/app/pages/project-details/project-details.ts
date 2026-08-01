3259859418df4013df48c8832ee094b3940d64bb
kristina <kristina.starovoit@gmail.com>
2026-08-01 12:46:21 +0200

    add: implement project details component with service integration

--- src/app/pages/project-details/project-details.ts
+++ src/app/pages/project-details/project-details.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, input, inject } from '@angular/core';
+import { ProjectsService } from '../../services/projects.service';
+import { Project } from '../../interfaces/project';
 
 @Component({
   selector: 'app-project-details',
@@ -6,4 +8,13 @@ import { Component } from '@angular/core';
   templateUrl: './project-details.html',
   styleUrl: './project-details.scss',
 })
-export class ProjectDetails {}
+export class ProjectDetails {
+  projectsService = inject(ProjectsService);
+  slug = input.required<string>();
+
+  //gibt das richtige Projekt zurück in abhängigkeit vom slug
+
+  project(): Project | undefined {
+    return this.projectsService.getBySlug(this.slug());
+  }
+}
