09f2c62429ffc876826ac973eecbee5768067b97
kristina <kristina.starovoit@gmail.com>
2026-08-01 12:54:08 +0200

    add: implement routing for project details

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { LegalNotice } from './pages/legal-notice/legal-notice';
 import { MainPage } from './pages/main/main-page/main-page';
 import { PrivacyPolicy } from './pages/privacy-policy/privacy-policy';
+import { ProjectDetails } from './pages/project-details/project-details';
 
 export const routes: Routes = [{
     path: '',
@@ -14,6 +15,10 @@ export const routes: Routes = [{
 {
     path: 'privacypolicy',
     component: PrivacyPolicy
+},
+{
+    path: 'project-detail/:slug',
+    component: ProjectDetails
 }
 
 ];
--- src/app/pages/main/components/projects/projects.ts
+++ src/app/pages/main/components/projects/projects.ts
@@ -1,9 +1,10 @@
 import { Component, inject } from '@angular/core';
 import { ProjectsService } from '../../../../services/projects.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-projects',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './projects.html',
   styleUrl: './projects.scss',
 })
