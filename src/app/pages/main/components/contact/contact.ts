5db76e3d0be28c826488c7796356b320bb667f6b
kristina <kristina.starovoit@gmail.com>
2026-08-05 18:00:13 +0200

    feat: enhance contact form with validation, error messages, and improved styling

--- src/app/pages/main/components/contact/contact.ts
+++ src/app/pages/main/components/contact/contact.ts
@@ -11,15 +11,32 @@ import { TranslatePipe } from '@ngx-translate/core';
   styleUrl: './contact.scss',
 })
 export class Contact {
-
   fb = inject(FormBuilder);
+
   contactForm = this.fb.group({
-    name: ['Your name goes here', [Validators.required, Validators.minLength(3)]],
-    email: ['youremail@email.com', [Validators.required, emailFormatValidator]],
-    message: ['Hello Kristina, I am interested in...', [Validators.required, Validators.minLength(10)]]
+    name: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, emailFormatValidator]],
+    message: ['', [Validators.required, Validators.minLength(10)]]
   });
+
+  isFormInvalid() {
+    return this.name?.invalid || this.email?.invalid || this.message?.invalid;
+  }
 
   submitForm() {
     console.log(this.contactForm.value);
+    this.contactForm.markAllAsTouched();
+  }
+
+  get name() {
+    return this.contactForm.get("name");
+  }
+
+  get email() {
+    return this.contactForm.get("email");
+  }
+
+  get message() {
+    return this.contactForm.get("message");
   }
 }
