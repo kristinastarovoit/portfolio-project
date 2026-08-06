c4ef1889b3507623d7f668b0da1519ed410efd7e
kristina <kristina.starovoit@gmail.com>
2026-08-06 12:17:13 +0200

    feat: add checkbox control to contact form with validation

--- src/app/pages/main/components/contact/contact.ts
+++ src/app/pages/main/components/contact/contact.ts
@@ -16,27 +16,33 @@ export class Contact {
   contactForm = this.fb.group({
     name: ['', [Validators.required, Validators.minLength(3)]],
     email: ['', [Validators.required, emailFormatValidator]],
-    message: ['', [Validators.required, Validators.minLength(10)]]
+    message: ['', [Validators.required, Validators.minLength(10)]],
+    checkbox: ['', [Validators.required]]
   });
 
   isFormInvalid() {
-    return this.name?.invalid || this.email?.invalid || this.message?.invalid;
-  }
+    return this.name?.invalid || this.email?.invalid || this.message?.invalid || this.checkbox?.invalid;
+  };
 
   submitForm() {
     console.log(this.contactForm.value);
     this.contactForm.markAllAsTouched();
-  }
+    this.contactForm.reset();
+  };
 
   get name() {
     return this.contactForm.get("name");
-  }
+  };
 
   get email() {
     return this.contactForm.get("email");
-  }
+  };
 
   get message() {
     return this.contactForm.get("message");
-  }
+  };
+
+  get checkbox() {
+    return this.contactForm.get("checkbox");
+  };
 }
