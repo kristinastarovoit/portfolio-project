e2d2dfb816eccc61b00b0c5db37cb772c69a9c5d
kristina <kristina.starovoit@gmail.com>
2026-08-08 11:35:34 +0200

    add: privacy policy and legal notice with translate function

--- src/app/pages/legal-notice/legal-notice.ts
+++ src/app/pages/legal-notice/legal-notice.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Footer } from '../../layout/footer/footer';
 import { Header } from "../../layout/header/header";
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-legal-notice',
-  imports: [Footer, Header],
+  imports: [Footer, Header, TranslatePipe],
   templateUrl: './legal-notice.html',
   styleUrl: './legal-notice.scss',
 })
--- src/app/pages/privacy-policy/privacy-policy.ts
+++ src/app/pages/privacy-policy/privacy-policy.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
 import { Header } from '../../layout/header/header';
 import { Footer } from '../../layout/footer/footer';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-privacy-policy',
-  imports: [Header, Footer],
+  imports: [Header, Footer, TranslatePipe],
   templateUrl: './privacy-policy.html',
   styleUrl: './privacy-policy.scss',
 })
-export class PrivacyPolicy {}
+export class PrivacyPolicy {
+}
