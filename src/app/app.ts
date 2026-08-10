a322b50ffe2b3701cadb922864736abf02c1de21
kristina <kristina.starovoit@gmail.com>
2026-08-10 13:38:24 +0200

    feat: save language to local storage

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header } from "./layout/header/header";
-import { Footer } from "./layout/footer/footer";
-import { Hero } from "./pages/main/components/hero/hero";
-import { About } from "./pages/main/components/about/about";
-import { Skills } from "./pages/main/components/skills/skills";
-import { Projects } from "./pages/main/components/projects/projects";
-import { Testimonials } from './pages/main/components/testimonials/testimonials';
-import { Contact } from "./pages/main/components/contact/contact";
-import { MainPage } from "./pages/main/main-page/main-page";
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -18,4 +10,8 @@ import { MainPage } from "./pages/main/main-page/main-page";
 })
 export class App {
   protected readonly title = signal('portfolio-project');
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem("language");
+    this.translate.use(savedLang?? "en");
+  }
 }
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -15,20 +15,21 @@ export class Header {
 
   currentLanguage = this.translate.currentLang;
 
-  ngOnInit() {
-    this.translate.use('en');
-  }
-
   switchLanguage() {
     if (this.currentLanguage() == "en") {
       this.translate.use("de");
+      localStorage.setItem("language", "de");
+      console.log("gespeichert:", localStorage.getItem("language"));
     } else if (this.currentLanguage() == "de") {
       this.translate.use("en");
+      localStorage.setItem("language", "en");
+      console.log("gespeichert:", localStorage.getItem("language"));
     }
   }
 
   switchToSetLanguage(language: string) {
     this.translate.use(language);
+    localStorage.setItem("language", language);
   }
 
   isMenuOpen = false;
